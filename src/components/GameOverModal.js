60e929b60a7b6e966912b70fc4d580e1dc5c848c
Serena X <84420380+xuserena12@users.noreply.github.com>
2023-12-22 20:02:00 -0500

    finished leaderboard

--- src/components/GameOverModal.js
+++ src/components/GameOverModal.js
@@ -1,9 +1,26 @@
-import { useState } from 'react';
 import './GameOverModal.css';
+import { useState, useEffect } from 'react';
+import { db } from '../config/firebase';
+import { collection, addDoc } from 'firebase/firestore';
 
 function GameOverModal( { handleCloseModal } ) {
-  let [isOpen, setIsOpen] = useState(true);
+  const [name, setName] = useState("");
+  const [time, setTime] = useState("");
 
+  const leaderboardCollectionRef = collection(db, 'leaderboard');
+
+  const addToLeaderboard = async () => {
+    try {
+      await addDoc(leaderboardCollectionRef, {
+        name: name,
+        time: time,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+    handleCloseModal();
+  };
+  
   return (
       <div className="modal-overlay">
         <div className="modal">
@@ -11,6 +28,10 @@ function GameOverModal( { handleCloseModal } ) {
             &times;
           </span>
           <p>You Win!</p>
+          <p>Please enter your name and time here:</p>
+          <input placeholder='Name ' onChange={(e) => setName(e.target.value)}/>
+          <input className="time-input "placeholder='Time' onChange={(e) => setTime(e.target.value)}/>
+          <buttom className="submit" onClick={addToLeaderboard}>Submit</buttom>
         </div>
     </div>
   );
--- src/pages/Leaderboard.js
+++ src/pages/Leaderboard.js
@@ -1,11 +1,37 @@
+import { useState, useEffect } from 'react';
+import { db } from '../config/firebase';
+import { collection, getDocs } from 'firebase/firestore';
+
 const Leaderboard = () => {
+  const [leaderboardList, setLeaderboardList] = useState([]);
+
+  const leaderboardCollectionRef = collection(db, "leaderboard");
+
+  useEffect(() => {
+    const getLeaderboardList = async () => {
+      try {
+        const data = await getDocs(leaderboardCollectionRef);
+        const filteredData = data.docs.map((doc) => ({
+          ...doc.data()
+        }));
+        setLeaderboardList(filteredData);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    getLeaderboardList();
+  }, []);
+
   return (
     <div>
-      <p>
-      Leaderboard page should go here!
-      </p>
+      <h1>Leaderboard</h1>
+      {leaderboardList.map((doc, index) => (
+        <h3 className="display" key={index}>{doc.name} - {doc.time}</h3>
+      ))}
     </div>
   );
+  
 }
 
 export default Leaderboard;
